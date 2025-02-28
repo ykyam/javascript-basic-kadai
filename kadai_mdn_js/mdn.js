d942316c0eba8dfb198a9cef9feef3033fff0d72
ykyam <kyaminuu@icloud.com>
2025-02-28 16:14:30 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const today = new Date();
+
+const year = today.getFullYear();
+const manth = today.getMonth() + 1;
+const date = today.getDate();
+
+console.log(year + '年' + manth + '月' + date +'日');
